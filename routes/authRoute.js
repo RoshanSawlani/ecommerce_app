27a060e1fa57a88d9c5134504b06ef9a73e7918b
RoshanSawlani <roshansawlani@gmail.com>
2024-04-16 11:59:43 +0530

    create register api

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import {registerController} from '../controllers/authController.js'
+import {loginController, registerController} from '../controllers/authController.js'
 // router object
 const router = express.Router()
 
 // routing
 // REGISTER || METHOD POST
 router.post('/register',registerController)
+
+// LOGIN || METHOD POST
+router.post('/login',loginController)
 
 export default router 
