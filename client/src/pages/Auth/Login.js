c7de43d13ffad7888ce639215330fa753b475b38
RoshanSawlani <roshansawlani@gmail.com>
2024-04-20 12:44:15 +0530

    create public and private routes

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 import toast from 'react-hot-toast';
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation} from 'react-router-dom';
 import "../../styles/AuthStyles.css";
 import { useAuth } from '../../context/auth';
 
@@ -11,6 +11,7 @@ const Login = () => {
     const [password, setPassword] = useState("")
     const [auth,setAuth]  = useAuth()
     const navigate = useNavigate()
+    const location = useLocation()
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
@@ -25,7 +26,7 @@ const Login = () => {
                     token:res.data.token
                 })
                 localStorage.setItem('auth',JSON.stringify(res.data))
-                navigate('/')
+                navigate(location.state || '/')
             } else {
                 toast.error(res.data.message)
             }
