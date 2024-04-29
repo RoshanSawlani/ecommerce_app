14ccadb3c9da71bc24aeda7e671e8c807beea22a
RoshanSawlani <roshansawlani@gmail.com>
2024-04-29 12:27:49 +0530

    create update user profile functionality

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -4,17 +4,17 @@ import { useCart } from '../context/cart'
 import { useAuth } from '../context/auth'
 import { useNavigate } from 'react-router-dom'
 const CartPage = () => {
-    const [auth,setAuth] = useAuth()
-    const [cart,setCart] = useCart()
+    const [auth, setAuth] = useAuth()
+    const [cart, setCart] = useCart()
     const navigate = useNavigate()
 
     // total price
-    const totalPrice = () =>{
+    const totalPrice = () => {
         try {
             let total = 0
-            cart?.map((item) => {total = total + item.price})
-            return total.toLocaleString("en-US",{
-                style:"currency",
+            cart?.map((item) => { total = total + item.price })
+            return total.toLocaleString("en-US", {
+                style: "currency",
                 currency: "USD"
             })
 
@@ -30,53 +30,70 @@ const CartPage = () => {
             let index = myCart.findIndex((item) => item._id === pid)
             myCart.splice(index, 1)
             setCart(myCart)
-            localStorage.setItem('cart',JSON.stringify(myCart))
+            localStorage.setItem('cart', JSON.stringify(myCart))
         } catch (error) {
             console.log(error)
         }
     }
 
-  return (
-    <Layout>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="text-center bg-light p-2 mb-1">
-                        {`Hello ${auth?.token && auth?.user?.name}`}
-                    </h1>
-                    <h4 className='text-center'>
-                        {cart?.length ? `You have ${cart.length} items in your cart ${auth?.token ? "" : "please login to checkout"}` : "Your cart is empty" }
-                    </h4>
+    return (
+        <Layout>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="text-center bg-light p-2 mb-1">
+                            {`Hello ${auth?.token && auth?.user?.name}`}
+                        </h1>
+                        <h4 className='text-center'>
+                            {cart?.length ? `You have ${cart.length} items in your cart ${auth?.token ? "" : "please login to checkout"}` : "Your cart is empty"}
+                        </h4>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8">
+                <div className="row">
+                    <div className="col-md-8">
                         {
-                            cart?.map((p)=>(
+                            cart?.map((p) => (
                                 <div className="row mb-2 p-3 card flex-row">
                                     <div className="col-md-4">
-                                    <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} width="100px" height={"100px"}/>
+                                        <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} width="100px" height={"100px"} />
                                     </div>
                                     <div className="col-md-8">
                                         <p>{p.name}</p>
-                                        <p>{p.description.substring(0,30)}</p>
+                                        <p>{p.description.substring(0, 30)}</p>
                                         <p>Price : {p.price}</p>
-                                        <button className='btn btn-danger' onClick={()=>removeCartItem(p._id)}>Remove</button>
-                                        </div>
+                                        <button className='btn btn-danger' onClick={() => removeCartItem(p._id)}>Remove</button>
+                                    </div>
                                 </div>
                             ))
                         }
                     </div>
-                <div className="col-md-4 text-center">
-                    <h2>Cart Summary</h2>
-                    <p>Total | Checkout | Payment</p>
-                    <hr />
-                    <h4>Total : {totalPrice()}</h4>
+                    <div className="col-md-4 text-center">
+                        <h2>Cart Summary</h2>
+                        <p>Total | Checkout | Payment</p>
+                        <hr />
+                        <h4>Total : {totalPrice()}</h4>
+                        {auth?.user?.address ? (
+                            <>
+                                <div className="mb-3">
+                                    <h4>Current Address</h4>
+                                    <h5>{auth.user?.address}</h5>
+                                    <button className='btn btn-outline-warning' onClick={() => navigate('/dashboard/user/profile')}>Update Address</button>
+                                </div>
+                            </>
+                        ) : (
+                            <div className="mb-3">
+                                {
+                                    auth?.token ? (
+                                        <button className='btn btn-outline-warning' onClick={() => navigate('/dashboard/user/profile')}>Update Address</button>
+                                    ) : (<button className='btn btn-outline-warning' onClick={() => navigate('/login',{state:'/cart',})}>Please Login to checkout</button>)
+                                }
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    </Layout>
-  )
+        </Layout>
+    )
 }
 
 export default CartPage
--- controllers/authController.js
+++ controllers/authController.js
@@ -153,3 +153,34 @@ export const forgotPasswordController = async(req,res) =>{
 export const testController = (req,res)=>{
     res.send("protected route")
 }
+
+// update profile
+export const updateProfileController = async(req,res) =>{
+    try {
+        const {name,email,password,address,phone} = req.body
+        const user = await userModel.findById(req.user._id)
+        // password
+        if(password && password.length < 6){
+            return res.json({error:"Password is required and must be 6 characters long"})
+        }
+        const hashedPassword = password ? await hashPassword(password) : undefined
+        const updatedUser = await userModel.findByIdAndUpdate(req.user._id,{
+            name:name || user.name,
+            password:hashedPassword || user.password,
+            phone:phone || user.phone,
+            address:address || user.address,
+        },{new:true})
+        res.status(200).send({
+            success:true,
+            message:"Profile updated successfully",
+            updatedUser,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Error while updating profile",
+            error
+        })
+    }
+}
